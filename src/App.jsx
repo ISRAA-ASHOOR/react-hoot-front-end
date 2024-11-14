1fdf5ab086805980704fe8860f39a072365fb75b
israa <esraashoor0112@gmail.com>
2024-11-14 16:35:20 +0300

    Completed hoot app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,77 @@
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import SignupForm from './components/SignupForm/SignupForm';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
 import SigninForm from './components/SigninForm/SigninForm';
 import * as authService from '../src/services/authService';
+import HootList from './components/HootList/HootList';
+import * as hootService from './services/hootService';
+import { useState, createContext, useEffect } from 'react';
+import HootDetails from './components/HootDetails/HootDetails';
+import HootForm from './components/HootForm/HootForm';
+
+export const AuthedUserContext = createContext(null);
 
 const App = () => {
   const [user, setUser] = useState(authService.getUser());
+  const [hoots, setHoots] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchAllHoots = async () => {
+      const hootsData = await hootService.index();
+      setHoots(hootsData);
+    };
+    if (user) fetchAllHoots();
+  }, [user]);
 
   const handleSignout = () => {
     authService.signout()
     setUser(null)
   };
 
+  const handleAddHoot = async (hootFormData) => {
+    const newHoot = await hootService.create(hootFormData);
+    setHoots([newHoot, ...hoots]);
+    navigate('/hoots');
+  };
+
+  const handleDeleteHoot = async (hootId) => {
+    const deletedHoot = await hootService.deleteHoot(hootId);
+    setHoots(hoots.filter((hoot) => hoot._id !== deletedHoot._id));
+    navigate('/hoots');
+  };
+
+  const handleUpdateHoot = async (hootId, hootFormData) => {
+    const updatedHoot = await hootService.update(hootId, hootFormData);
+    setHoots(hoots.map((hoot) => (hootId === hoot._id ? updatedHoot : hoot)));
+    navigate(`/hoots/${hootId}`);
+  };
+
   return (
     <>
+     <AuthedUserContext.Provider value={user}>
     <NavBar user={user} handleSignout={handleSignout} />
     <Routes>
-      {
-        user ?
-          <Route path="/" element={<Dashboard user={user} />} />
-        :
-          <Route path="/" element={<Landing />} />
-      }
+      {user ? (
+      // Protected Routes:
+      <>
+        <Route path="/" element={<Dashboard user={user} />} />
+        <Route path="/hoots" element={<HootList hoots={hoots} />} />
+        <Route path="/hoots/:hootId" element={<HootDetails handleDeleteHoot={handleDeleteHoot} />} />
+        <Route path="/hoots/new" element={<HootForm handleAddHoot={handleAddHoot} />} />
+        <Route path="/hoots/:hootId/edit" element={<HootForm handleUpdateHoot={handleUpdateHoot} />} />
+      </>
+    ) : (
+      // Public Route:
+      <Route path="/" element={<Landing />} />
+    )}
 
     <Route path="/signup" element={<SignupForm setUser={setUser} />} />
     <Route path='/signin' element={<SigninForm setUser={setUser} />} /> 
     </Routes>
+    </AuthedUserContext.Provider>
     </>
   );
 };
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,23 @@
 import { Link } from 'react-router-dom';
+import styles from './NavBar.module.css';
+import Logo from '../../assets/images/logo.svg';
 
 const NavBar = ({ user, handleSignout }) => {
     return (
       <>
         { user ? (
-          <nav>
+          <nav className={styles.container}>
+            <Link to='/'><img src={Logo} alt="A cute owl" /></Link>
             <ul>
               <li><Link to="/">Home</Link></li>
+              <li><Link to='/hoots'>HOOTS</Link></li>
+              <li><Link to="/hoots/new">NEW HOOT</Link></li>
               <li><Link to="" onClick={handleSignout}>Sign Out</Link></li>
             </ul>
           </nav>
         ) : (
-          <nav>
+          <nav className={styles.container}>
+            <Link to='/'><img src={Logo} alt="A cute owl" /></Link>
             <ul>
               <li><Link to="/signin">Sign In</Link></li>
               <li><Link to="/signup">Sign Up</Link></li>
